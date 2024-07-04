7d6cf6167dc87163b1df24ae34ce4f54196e7c8d
seamuddin <seamuddin2020@gmail.com>
2024-07-05 02:59:11 +0600

    Logs implemented

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
 Route::get('/marketplace', function () {
     return view('marketplace');
 });
+
+Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
