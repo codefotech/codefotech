5eade15139213026d9c1d112c86b706dd5e88b2e
Mainur Rahaman <mainuromayer@gmail.com>
2024-07-29 18:09:39 +0600

    laravel file setup and project architechture change

---
+++ resources/views/frontend/layouts/about.blade.php
@@ -0,0 +1,116 @@
+<!-- ======= About Section Start ======= -->
+<section id="about" class="about container-fluid pt-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-7">
+                <div class="section-title-2 position-relative pb-3 mb-5">
+                    <h5 class="fw-bold codefotech_text_color text-uppercase">About Us</h5>
+                    <h1 class="mb-0">The Best IT Solution With 10 Years of Experience</h1>
+                </div>
+                <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet
+                    diam et eos labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita
+                    duo justo et tempor eirmod magna dolore erat amet</p>
+                <div class="border rounded p-4 wow fadeInUp" data-wow-delay="0.6s">
+                    <nav>
+                        <div class="nav nav-tabs mb-3" id="nav-tab" role="tablist">
+                            <button class="nav-link text-uppercase active" id="nav-story-tab" data-bs-toggle="tab"
+                                    data-bs-target="#nav-story" type="button" role="tab" aria-controls="nav-story"
+                                    aria-selected="true">Story
+                            </button>
+
+                            <button class="nav-link text-uppercase" id="nav-reviews-tab" data-bs-toggle="tab"
+                                    data-bs-target="#nav-review" type="button" role="tab" aria-controls="nav-review"
+                                    aria-selected="false">Review
+                            </button>
+                        </div>
+                    </nav>
+
+                    <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="nav-story" role="tabpanel"
+                             aria-labelledby="nav-story-tab">
+                            <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet
+                                diam et eos labore. Clita erat ipsum et lorem et sit</p>
+                            <div class="row g-3">
+                                <div class="col-sm-6">
+                                    <h5 class="mb-3"><i class="fa fa-check codefotech_text_color me-3"></i>Award Winning</h5>
+                                    <h5 class="mb-0"><i class="fa fa-check codefotech_text_color me-3"></i>Professional Staff
+                                    </h5>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h5 class="mb-3"><i class="fa fa-check codefotech_text_color me-3"></i>24/7 Support</h5>
+                                    <h5 class="mb-0"><i class="fa fa-check codefotech_text_color me-3"></i>Fair Prices</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="nav-reviews" role="tabpanel" aria-labelledby="nav-reviews-tab">
+                            <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet
+                                diam et eos labore. Clita erat ipsum et lorem et sit</p>
+                            <div class="row g-3">
+                                <div class="col-sm-6">
+                                    <h5 class="mb-3"><i class="fa fa-check codefotech_text_color me-3"></i>Award Winning</h5>
+                                    <h5 class="mb-0"><i class="fa fa-check codefotech_text_color me-3"></i>Professional Staff
+                                    </h5>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h5 class="mb-3"><i class="fa fa-check codefotech_text_color me-3"></i>24/7 Support</h5>
+                                    <h5 class="mb-0"><i class="fa fa-check codefotech_text_color me-3"></i>Fair Prices</h5>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5" style="min-height: 400px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s"
+                         src="{{ asset('assets-1/img/about.jpg') }}" style="object-fit: cover;">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="container-fluid facts pb-5 pt-lg-0 mt-0 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container py-5 pt-lg-0">
+        <div class="row gx-0">
+            <div class="col-lg-4 wow zoomIn" data-wow-delay="0.1s">
+                <div class="codefotech_color d-flex align-items-center justify-content-center p-4" style="height: 150px;">
+                    <div class="bg-white d-flex align-items-center justify-content-center rounded mb-2"
+                         style="width: 60px; height: 60px;">
+                        <i class="fa fa-users codefotech_text_color"></i>
+                    </div>
+                    <div class="ps-4">
+                        <h5 class="text-white mb-0">Happy Clients</h5>
+                        <h1 class="text-white mb-0" data-toggle="counter-up">12345</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 wow zoomIn" data-wow-delay="0.3s">
+                <div class="bg-dark d-flex align-items-center justify-content-center p-4" style="height: 150px;">
+                    <div class="bg-white d-flex align-items-center justify-content-center rounded mb-2"
+                         style="width: 60px; height: 60px;">
+                        <i class="fa fa-check codefotech_text_color"></i>
+                    </div>
+                    <div class="ps-4">
+                        <h5 class="text-white mb-0">Projects Done</h5>
+                        <h1 class="text-white mb-0" data-toggle="counter-up">12345</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 wow zoomIn" data-wow-delay="0.6s">
+                <div class="codefotech_color d-flex align-items-center justify-content-center p-4" style="height: 150px;">
+                    <div class="bg-white d-flex align-items-center justify-content-center rounded mb-2"
+                         style="width: 60px; height: 60px;">
+                        <i class="fa fa-award codefotech_text_color"></i>
+                    </div>
+                    <div class="ps-4">
+                        <h5 class="text-white mb-0">Win Awards</h5>
+                        <h1 class="text-white mb-0" data-toggle="counter-up">12345</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ======== About Section End ======== -->
---
+++ resources/views/frontend/layouts/services.blade.php
@@ -0,0 +1,98 @@
+<!-- ======= Services Section Start ======= -->
+<section id="services" class="services container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5 mb-5">
+            <div class="col-lg-5" style="min-height: 400px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.3s"
+                         src="{{ asset('assets-1/img/feature.jpg') }}" style="object-fit: cover;">
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="section-title-2 position-relative pb-3 mb-5">
+                    <h5 class="fw-bold codefotech_text_color text-uppercase">Our Services</h5>
+                    <h1 class="mb-0">Custom IT Solutions for Your Successful Business</h1>
+                </div>
+                <div class="row g-5">
+                    <div class="col-md-6 wow zoomIn" data-wow-delay="0.6s">
+                        <div
+                            class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                            <div class="service-icon">
+                                <i class="fa fa-user-md text-white"></i>
+                            </div>
+                            <h4 class="mb-3">Cyber Security</h4>
+                            <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum
+                                dolore
+                                sed</p>
+                            <a class="btn btn-lg codefotech_color rounded" href>
+                                <i class="bi bi-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 wow zoomIn" data-wow-delay="0.9s">
+                        <div
+                            class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                            <div class="service-icon">
+                                <i class="fa fa-user-md text-white"></i>
+                            </div>
+                            <h4 class="mb-3">Data Analytics</h4>
+                            <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum
+                                dolore
+                                sed</p>
+                            <a class="btn btn-lg codefotech_color rounded" href>
+                                <i class="bi bi-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row g-5 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="col-lg-7">
+                <div class="row g-5">
+                    <div class="col-md-6 wow zoomIn" data-wow-delay="0.3s">
+                        <div
+                            class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                            <div class="service-icon">
+                                <i class="fa fa-user-md text-white"></i>
+                            </div>
+                            <h4 class="mb-3">Web Development</h4>
+                            <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum
+                                dolore
+                                sed</p>
+                            <a class="btn btn-lg codefotech_color rounded" href>
+                                <i class="bi bi-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 wow zoomIn" data-wow-delay="0.6s">
+                        <div
+                            class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
+                            <div class="service-icon">
+                                <i class="fa fa-user-md text-white"></i>
+                            </div>
+                            <h4 class="mb-3">SEO Optimization</h4>
+                            <p class="m-0">Amet justo dolor lorem kasd amet magna sea stet eos vero lorem ipsum
+                                dolore
+                                sed</p>
+                            <a class="btn btn-lg codefotech_color rounded" href>
+                                <i class="bi bi-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 wow zoomIn" data-wow-delay="0.9s">
+                <div
+                    class="position-relative codefotech_color rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
+                    <h3 class="text-white mb-3">Call Us For A Quote</h3>
+                    <p class="text-white mb-3">Clita ipsum magna kasd rebum at ipsum amet dolor justo dolor est
+                        magna
+                        stet eirmod</p>
+                    <h2 class="text-white mb-0">+880 1896086540</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ======== Services Section End ======== -->
