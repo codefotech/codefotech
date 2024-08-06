09b1dea6d85bc4bcedbcdf95e65e1e1493b44a84
Mainur Rahaman <mainuromayer@gmail.com>
2024-08-06 21:04:53 +0600

    update

--- app/Modules/Blog/routes/web.php
+++ app/Modules/Blog/routes/web.php
@@ -3,14 +3,12 @@
 use App\Modules\Blog\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
-Route::group( array('Module' => 'Blog'), function () {
+Route::group( array( 'Module' => 'Blog' ), function () {
     Route::prefix( 'blog' )->group( function () {
         Route::get( '/', [BlogController::class, 'index'] )->name( 'blog.index' );
-        Route::get( '/create', [BlogController::class, 'create'] )->name( 'blog.create' );
-        Route::post( '/', [BlogController::class, 'store'] )->name( 'blog.store' );
-        Route::get( '/{id}', [BlogController::class, 'show'] )->name( 'blog.show' );
-        Route::get( '/{id}/edit', [BlogController::class, 'edit'] )->name( 'blog.edit' );
-        Route::put( '/{id}', [BlogController::class, 'update'] )->name( 'blog.update' );
-        Route::delete( '/{id}', [BlogController::class, 'destroy'] )->name( 'blog.destroy' );
+        Route::match( array( 'get', 'post' ), '/list', array( BlogController::class, 'list' ) )->name( 'account.list' );
+        Route::get( 'create', array( BlogController::class, 'create' ) )->name( 'account.create' );
+        Route::post( 'store', array( BlogController::class, 'store' ) )->name( 'account.store' );
+        Route::get( 'edit/{id}', array( BlogController::class, 'edit' ) )->name( 'account.edit' );
     } );
 } );
